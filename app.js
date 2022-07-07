c21905f1cb90730c6b94fe58bcbf3a4cfc01d9c2
OsmandLN <osmand8370@gmail.com>
2022-07-08 02:49:44 +0800

    Fixed: adjusted the port.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // require express.js
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // require express-handlebars
 const exphbs = require('express-handlebars')
