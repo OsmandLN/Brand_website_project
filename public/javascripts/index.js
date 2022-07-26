297302af3c1bac36bf6d674c1507c62cf1cd7d31
OsmandLN <osmand8370@gmail.com>
2022-07-26 14:14:27 +0800

    Feat: added handler when contact form is submitted.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,16 @@
-// slogan slide-in effect
-const brandValue = document.querySelector('#brand-value')
 
+const brandValue = document.querySelector('#brand-value')
+const submitBtn = document.querySelector('.submit-btn')
+const leaveAMessage = document.querySelector('#leave-a-message')
+
+// slogan slide-in effect
 window.addEventListener('load', function sloganSlideInEffectAdded() {
   brandValue.classList.add('slide-in')
+})
+
+
+submitBtn.addEventListener('click', () => {
+  // clear the form after submitted
+  leaveAMessage.reset()
+  alert('Message submitted!')
 })
